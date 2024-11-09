63062ce77bd5321b2245b738356107974cc1a476
Max batleforc <maxleriche.60@gmail.com>
2024-11-09 01:59:06 +0100

    feat: change owner

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -30,6 +30,8 @@ backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 
+backend.add(import('@backstage/plugin-events-backend'));
+
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
 
