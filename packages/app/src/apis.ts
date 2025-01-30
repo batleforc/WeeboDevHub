63c1f1e1118db48ef48d16e96151923b8162e10f
Max batleforc <maxleriche.60@gmail.com>
2025-01-30 23:17:12 +0100

    feat: add in theory umami

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -7,13 +7,25 @@ import {
   AnyApiFactory,
   configApiRef,
   createApiFactory,
+  analyticsApiRef,
+  fetchApiRef,
 } from '@backstage/core-plugin-api';
+import { UmamiAnalytics } from '@axis-backstage/plugin-analytics-module-umami';
 
 export const apis: AnyApiFactory[] = [
   createApiFactory({
     api: scmIntegrationsApiRef,
     deps: { configApi: configApiRef },
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
+  }),
+  createApiFactory({
+    api: analyticsApiRef,
+    deps: {
+      configApi: configApiRef,
+      fetchApi: fetchApiRef,
+    },
+    factory: ({ configApi, fetchApi }) =>
+      UmamiAnalytics.fromConfig(configApi, { fetchApi }),
   }),
   ScmAuth.createDefaultApiFactory(),
 ];
